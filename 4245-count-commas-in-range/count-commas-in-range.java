076cd8dd2820541b536c3fa1bda06561fb815e75
Niyamat Barote <niyamatbarote@gmail.com>
2026-09-08 22:15:56 +0530

    Time: 1 ms (83.65%) | Memory: 42.6 MB (39.85%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -4,14 +4,7 @@ class Solution {
         if (n<1000) {
             return 0;
         }
-        
-        long total = 0;
-        long p = 1000L;
 
-        while (p <= n) {
-            total += n - p + 1;
-            p *= 1000;
-        }
-        return (int) total;
+        return n - 999;
     }
 }
