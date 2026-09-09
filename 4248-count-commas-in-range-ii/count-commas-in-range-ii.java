bcf2284a368f3954eafdc9070e730d16c804230d
Niyamat Barote <niyamatbarote@gmail.com>
2026-09-09 21:17:25 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (98.72%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -1,8 +1,6 @@
 class Solution {
     public long countCommas(long n) {
-        if (n<0) {
-            return 0;
-        }
+        
         long total = 0;
         long p = 1000l;
         while(p<=n) {
